169f6a44dbab47100549c0bdd4d5c8c00af7a4b0
jeremyfelt <jeremy.felt@gmail.com>
2015-06-10 09:40:03 -0700

    Remove unneeded functions.php for now

--- functions.php
+++
@@ -1,7 +0,0 @@
-<?php // Opening PHP tag - nothing should be before this, not even whitespace
-
-// Custom Function to Include
-function favicon_link() {
-    echo '<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />' . "\n";
-}
-add_action( 'wp_head', 'favicon_link' );
