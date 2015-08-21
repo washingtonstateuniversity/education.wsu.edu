6ac5e43bc00a35a4b27655ad6441d2c10d3f0411
jbickelhaupt <jbickelhaupt@wsu.edu>
2015-08-21 15:03:33 -0700

    Alternative Landing Page
    adding additional color palette option for alternative landing pages

--- functions.php
+++ functions.php
@@ -10,6 +10,7 @@ function wsu_education_color_palette_values() {
 	return array(
 		'default' => array( 'name' => 'Blue',  'hex' => '#3e8b94' ),
 		'yellow' => array( 'name' => 'Yellow',  'hex' => '#c69214' ),
+		'orange' => array( 'name' => 'Orange',  'hex' => '#ddbea1' ),
 	);
 }
 
