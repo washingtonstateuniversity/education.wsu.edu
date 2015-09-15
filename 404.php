e22e15e7f5dc233df0704203039e9c2fd146053a
jbickelhaupt <jbickelhaupt@wsu.edu>
2015-09-15 10:03:56 -0700

    custom 404 changes
    changing the text and general layout/appearance of custom 404 page

--- 404.php
+++ 404.php
@@ -11,18 +11,12 @@
 				<article id="post-0" class="post error404 no-results not-found">
 
 					<header class="article-header">
-						<h1 class="article-title">Page Not Found</h1>
+						<h1 class="article-title">404</h1>
+						<h1 class="article-subtitle">Page Not Found</h1>						
 					</header>
-					
-					<div class="msg404">
-					<h1>404 error?</h1>
-					<h2>Cheer up!</h2>
-					<h3>It could be worse&hellip;<br>you could look like <em><strong>this</strong></em> clown.</h3>
-					</div>
-					
+
 					<div class="entry-content">
-						<h5>We take your web experience seriously&mdash;even if we don't take ourselves seriously.</h5>
-						<p>Please feel free to search <a href="https://education.wsu.edu/">our site</a> to find what you were originally looking for. You can also <a href="mailto:education@wsu.edu">email us</a>.</p>
+						<p>We&rsquo;re sorry. The page you were looking for may have been moved or deleted. Please feel free to search <a href="https://education.wsu.edu/">our site</a> to find what you were originally looking for. You can also <a href="mailto:education@wsu.edu">email us</a>.</p>
 						<?php get_search_form(); ?>
 					</div><!-- .entry-content -->
 
